d058f63b1ee1057d58c4769343439cb796f55ecf
Alexander Guttenberger <contact.guttenberger@gmail.com>
2024-01-10 13:36:29 +0000

    improve parameter handling

--- jobs/mapreduce/src/main/java/com/guttenberger/app/App.java
+++ jobs/mapreduce/src/main/java/com/guttenberger/app/App.java
@@ -7,10 +7,18 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+        
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: wordcount <in> <out>");
+            System.exit(2);
+        }
+
         Job job = Job.getInstance(conf, "word count");
         job.setJarByClass(App.class);
         job.setMapperClass(MyMapper.class);
@@ -18,8 +26,8 @@ public class App {
         job.setReducerClass(MyReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
