38bdbfcb7a2fd88e9cff5b1677b89d94ff723b61
Alexander Guttenberger <contact.guttenberger@gmail.com>
2024-04-26 10:54:51 +0000

    rename and make test bin runnable

--- deployment/scripts/extract-hadoop-bins.sh
+++ deployment/scripts/extract-hadoop-bins.sh
@@ -2,6 +2,8 @@
 tar -xzvf /usr/hdp/current/hadoop-client/mapreduce.tar.gz \
     --wildcards \
     --strip-components=4 \
-    -C ~/ \
-    'hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar' \
+    -C /usr/hdp/current/hadoop-client/examples/ \
     'hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-*-tests.jar'
+
+mv /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-*-tests.jar /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-tests.jar
+chmod +x /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-tests.jar
