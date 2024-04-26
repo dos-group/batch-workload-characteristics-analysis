5ad6ee99c187264a2cb785d2f52652e4ad93dcfa
Alexander Guttenberger <contact.guttenberger@gmail.com>
2024-04-26 11:51:41 +0000

    fix script

--- deployment/scripts/extract-hadoop-bins.sh
+++ deployment/scripts/extract-hadoop-bins.sh
@@ -1,9 +1,9 @@
 # extract hadoop binaries for hibench
-tar -xzvf /usr/hdp/current/hadoop-client/mapreduce.tar.gz \
+sudo tar -xzvf /usr/hdp/current/hadoop-client/mapreduce.tar.gz \
     --wildcards \
     --strip-components=4 \
     -C /usr/hdp/current/hadoop-client/examples/ \
     'hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-*-tests.jar'
 
-mv /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-*-tests.jar /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-tests.jar
-chmod +x /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-tests.jar
+sudo mv /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-*-tests.jar /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-tests.jar
+sudo chmod +x /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-tests.jar
