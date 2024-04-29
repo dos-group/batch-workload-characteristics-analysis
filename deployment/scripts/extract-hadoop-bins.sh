09efcd343ae47d77f29e9ff1015ee7ad1f5102fa
Alexander Guttenberger <contact.guttenberger@gmail.com>
2024-04-29 13:13:16 +0000

    fix paths

--- deployment/scripts/extract-hadoop-bins.sh
+++ deployment/scripts/extract-hadoop-bins.sh
@@ -2,8 +2,8 @@
 sudo tar -xzvf /usr/hdp/current/hadoop-client/mapreduce.tar.gz \
     --wildcards \
     --strip-components=4 \
-    -C /usr/hdp/current/hadoop-client/examples/ \
+    -C /usr/hdp/current/hadoop-mapreduce-client \
     'hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-*-tests.jar'
 
-sudo mv /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-*-tests.jar /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-tests.jar
-sudo chmod +x /usr/hdp/current/hadoop-client/examples/hadoop-mapreduce-client-jobclient-tests.jar
+sudo mv /usr/hdp/current/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient-*-tests.jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient-tests.jar
+sudo chmod +x /usr/hdp/current/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient-tests.jar
