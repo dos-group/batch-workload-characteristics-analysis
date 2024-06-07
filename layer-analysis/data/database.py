5e2fef872b1d55817689f63b5f468261136756d3
Alexander Guttenberger <contact.guttenberger@gmail.com>
2024-06-07 09:27:29 +0000

    refator: share single dataset

---
+++ layer-analysis/data/database.py
@@ -0,0 +1,73 @@
+import pandas as pd
+# Ensure you import the count_tuples function correctly
+from .helper_fns import count_tuples
+
+# Load the CSV file
+data = pd.read_csv('data/layers.csv')
+# Assign appropriate headers to the dataframe
+data.columns = ["Title", "Year", "Layers"]
+
+two_tupel_combinations = count_tuples(data, 2)
+
+three_tupel_combinations = count_tuples(data, 3)
+
+
+data_layer_combinations = {
+    ("Hardware Layer", "Virtualisation Layer", "Performance Layer", "Data Layer"): 1,
+    ("Hardware Layer", "Virtualisation Layer", "Performance Layer", "Data Layer", "User Application Layer"): 1,
+    ("Hardware Layer", "Big Data Framework Layer"): 3,
+    ("Hardware Layer", "Big Data Framework Layer", "Performance Layer"): 4,
+    ("Hardware Layer", "Big Data Framework Layer", "Performance Layer", "Data Layer", "User Application Layer"): 3,
+    ("Hardware Layer", "Big Data Framework Layer", "Data Layer"): 5,
+    ("Hardware Layer", "Big Data Framework Layer", "Data Layer", "User Application Layer"): 2,
+    ("Hardware Layer", "Big Data Framework Layer", "User Application Layer"): 2,
+    ("Hardware Layer", "Performance Layer"): 5,
+    ("Hardware Layer", "Performance Layer", "Data Layer"): 5,
+    ("Hardware Layer", "Performance Layer", "User Application Layer"): 1,
+    ("Hardware Layer", "Data Layer"): 1,
+    ("Hardware Layer", "Virtualisation Layer"): 2,
+    ("Virtualisation Layer", "Big Data Framework Layer", "Performance Layer"): 1,
+    ("Virtualisation Layer", "Big Data Framework Layer", "Performance Layer", "Data Layer"): 1,
+    ("Big Data Framework Layer",): 4,
+    ("Big Data Framework Layer", "Performance Layer"): 4,
+    ("Big Data Framework Layer", "Performance Layer", "Data Layer"): 1,
+    ("Big Data Framework Layer", "Performance Layer", "User Application Layer"): 1,
+    ("Big Data Framework Layer", "Data Layer"): 4,
+    ("Big Data Framework Layer", "Data Layer", "User Application Layer"): 1,
+    ("Big Data Framework Layer", "User Application Layer"): 2,
+    ("Big Data Framework Layer", "Virtualisation Layer"): 1,
+    ("Performance Layer",): 4,
+    ("Performance Layer", "Data Layer"): 1,
+    ("Data Layer", "User Application Layer"): 1,
+}
+
+## old data with JVM and Cloud Layer
+old_data = {
+    ("Hardware Layer", "JVM Layer", "Performance Layer", "Data Layer"): 1,
+    ("Hardware Layer", "JVM Layer", "Performance Layer", "Data Layer", "User Application Layer"): 1,
+    ("Hardware Layer", "Big Data Framework Layer"): 3,
+    ("Hardware Layer", "Big Data Framework Layer", "Performance Layer"): 4,
+    ("Hardware Layer", "Big Data Framework Layer", "Performance Layer", "Data Layer", "User Application Layer"): 3,
+    ("Hardware Layer", "Big Data Framework Layer", "Data Layer"): 5,
+    ("Hardware Layer", "Big Data Framework Layer", "Data Layer", "User Application Layer"): 2,
+    ("Hardware Layer", "Big Data Framework Layer", "User Application Layer"): 2,
+    ("Hardware Layer", "Performance Layer"): 5,
+    ("Hardware Layer", "Performance Layer", "Data Layer"): 5,
+    ("Hardware Layer", "Performance Layer", "User Application Layer"): 1,
+    ("Hardware Layer", "Data Layer"): 1,
+    ("Hardware Layer", "Cloud Layer"): 1,
+    ("Hardware Layer", "Virtualisation Layer"): 1,
+    ("JVM Layer", "Big Data Framework Layer", "Performance Layer"): 1,
+    ("JVM Layer", "Big Data Framework Layer", "Performance Layer", "Data Layer", "Cloud Layer"): 1,
+    ("Big Data Framework Layer",): 4,
+    ("Big Data Framework Layer", "Performance Layer"): 4,
+    ("Big Data Framework Layer", "Performance Layer", "Data Layer"): 1,
+    ("Big Data Framework Layer", "Performance Layer", "User Application Layer"): 1,
+    ("Big Data Framework Layer", "Data Layer"): 4,
+    ("Big Data Framework Layer", "Data Layer", "User Application Layer"): 1,
+    ("Big Data Framework Layer", "User Application Layer"): 2,
+    ("Big Data Framework Layer", "Virtualisation Layer"): 1,
+    ("Performance Layer",): 4,
+    ("Performance Layer", "Data Layer"): 1,
+    ("Data Layer", "User Application Layer"): 1,
+}
--- layer-analysis/data/helper_fns.py
+++ layer-analysis/data/helper_fns.py
@@ -2,27 +2,18 @@
 from collections import Counter
 from itertools import combinations
 
-from database import data
-
 def count_tuples(data, tuple_length):
+    layers_list = data['Layers'].apply(lambda x: x.split(', '))
     tuple_frequencies = Counter()
 
-    for combination, count in data.items():
+    for combination in layers_list:
         if len(combination) >= tuple_length:  # Consider only combinations with at least the desired length
             for tuple_combination in combinations(combination, tuple_length):
-                tuple_frequencies[tuple_combination] += count
+                # Sort the tuple to ensure order is not important
+                sorted_tuple = tuple(sorted(tuple_combination))
+                tuple_frequencies[sorted_tuple] += 1
 
     # Find the most common tuples
     most_common_tuples = tuple_frequencies.most_common()
 
     return most_common_tuples
-
-# counting for 3-tuples
-most_common_three_tuples = count_tuples(data, 3)
-for tuple in most_common_three_tuples:
-    print(tuple)
-
-# counting for 2-tuples
-# most_common_two_tuples = count_tuples(data, 2)
-# for tuple in most_common_two_tuples:
-#     print(tuple)
